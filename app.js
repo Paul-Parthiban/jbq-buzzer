d8e71e5360f10d92f6a0d5c40efc6c5f6d04bb68
Paul-Parthiban <paulrichard.parthiban@gmail.com>
2021-09-18 10:33:53 -0400

    JBQ Buzzer
    Buzzer for JBQ

---
+++ app.js
@@ -0,0 +1,26 @@
+const buzzer = document.getElementsByTagName("BODY")[0];
+const timer = document.querySelector(".timer span");
+var beep = document.getElementById("beep");
+var buzzerBeep = document.getElementById("buzzer-beep")
+let timerSecond = 30;
+
+
+document.addEventListener("click", function(){
+    buzzerBeep.play();
+
+    timer.innerHTML = `${timerSecond}`;
+
+    const countDown = setInterval(() => {
+        timerSecond--;
+        timer.innerHTML = `${timerSecond}`;
+
+        if (timerSecond <= 0 || timerSecond < 1) {
+            clearInterval(countDown)
+            beep.play()
+            setInterval(() => {
+                timer.innerHTML = "3"
+                this.location.reload()
+            }, 3000)
+        }
+    }, 1000);
+});
